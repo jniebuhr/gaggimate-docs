d7da1be146ec004a0002dbd880f56119c168aea2
Jochen Niebuhr <jniebuhr@cfire.de>
2024-12-19 17:03:45 +0100

    chore: Push firmware v1.0.1

--- scripts/update-firmware.sh
+++ scripts/update-firmware.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-curl -sL -o public/firmware/controller/esp32-s3/bootloader.bin https://github.com/jniebuhr/gaggimate/releases/download/v1.0.0/controller-bootloader.bin
-curl -sL -o public/firmware/controller/esp32-s3/partitions.bin https://github.com/jniebuhr/gaggimate/releases/download/v1.0.0/controller-partitions.bin
-curl -sL -o public/firmware/controller/esp32-s3/firmware.bin https://github.com/jniebuhr/gaggimate/releases/download/v1.0.0/controller-firmware.bin
+version="v1.0.1"
 
-curl -sL -o public/firmware/display/esp32-s3/bootloader.bin https://github.com/jniebuhr/gaggimate/releases/download/v1.0.0/display-bootloader.bin
-curl -sL -o public/firmware/display/esp32-s3/partitions.bin https://github.com/jniebuhr/gaggimate/releases/download/v1.0.0/display-partitions.bin
-curl -sL -o public/firmware/display/esp32-s3/firmware.bin https://github.com/jniebuhr/gaggimate/releases/download/v1.0.0/display-firmware.bin
-curl -sL -o public/firmware/display/esp32-s3/filesystem.bin https://github.com/jniebuhr/gaggimate/releases/download/v1.0.0/display-filesystem.bin
+curl -sL -o public/firmware/controller/esp32-s3/bootloader.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/controller-bootloader.bin
+curl -sL -o public/firmware/controller/esp32-s3/partitions.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/controller-partitions.bin
+curl -sL -o public/firmware/controller/esp32-s3/firmware.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/controller-firmware.bin
+
+curl -sL -o public/firmware/display/esp32-s3/bootloader.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/display-bootloader.bin
+curl -sL -o public/firmware/display/esp32-s3/partitions.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/display-partitions.bin
+curl -sL -o public/firmware/display/esp32-s3/firmware.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/display-firmware.bin
+curl -sL -o public/firmware/display/esp32-s3/filesystem.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/display-filesystem.bin
