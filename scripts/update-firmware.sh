3bce88b172ead8edf2d6aa32e759aea3604feb75
Jochen Ullrich <kontakt@ju-hh.de>
2025-05-24 10:31:11 +0200

    feat: Update to v1.4.4

--- scripts/update-firmware.sh
+++ scripts/update-firmware.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-version="v1.4.2"
+version="v1.4.4"
 
-curl -sL -o public/firmware/controller/esp32-s3/bootloader.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/controller-bootloader.bin
-curl -sL -o public/firmware/controller/esp32-s3/partitions.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/controller-partitions.bin
-curl -sL -o public/firmware/controller/esp32-s3/firmware.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/controller-firmware.bin
+curl -sL -o public/firmware/controller/esp32-s3/bootloader.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/board-bootloader.bin
+curl -sL -o public/firmware/controller/esp32-s3/partitions.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/board-partitions.bin
+curl -sL -o public/firmware/controller/esp32-s3/firmware.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/board-firmware.bin
 
 curl -sL -o public/firmware/display/esp32-s3/bootloader.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/display-bootloader.bin
 curl -sL -o public/firmware/display/esp32-s3/partitions.bin https://github.com/jniebuhr/gaggimate/releases/download/$version/display-partitions.bin
