697f13fea1d1e745415535f41608ef215e9bf907
Jay R. <jayblah@gmail.com>
2026-03-04 22:03:14 -0600

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,13 @@ import remarkToc from 'remark-toc';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      nesting: true
-    }),
-    mdx()
-  ],
+  integrations: [tailwind({
+    nesting: true
+  }), mdx(), react()],
   markdown: {
     remarkPlugins: [[remarkToc, {maxDepth: 2}]],
   }
